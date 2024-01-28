c5c33bed017ec322b4f07760f54b1f2ea04fa430
Šime Vidas <sime.vidas@gmail.com>
2024-01-28 18:26:41 +0100

    Remove test character

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Šime’s blog.',
+  title: 'Šime’s blog',
   subtitle: 'I’m interested in HTML, CSS, and making the web less annoying',
   language: 'en',
   url: 'https://šime.eu/',
